005cf8abd04ea88f3de58f99b7823670ca08469a
Prenaphan Krudthong <1163109050224@mail.rmutt.ac.th>
2024-02-26 18:44:37 +0700

    อัพเดทฟังก์ชั่น Bootstrap at main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import serverless = require('serverless-http');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe())
-  app.enableCors();
-  await app.listen(3000);
-  console.log("LET'S GO")
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = '.netlify/functions/main';
+  app.setGlobalPrefix(globalPrefix);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp)
 }
-bootstrap();
+
+let server;
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {cors: true});
+//   app.setGlobalPrefix('api');
+//   app.useGlobalPipes(new ValidationPipe())
+//   app.enableCors();
+//   await app.listen(3000);
+//   console.log("LET'S GO")
+// }
+// bootstrap();
