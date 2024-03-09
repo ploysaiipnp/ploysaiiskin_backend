74bfea08bc59cd4562ddfabad86511eb35c01f18
Prenaphan Krudthong <1163109050224@mail.rmutt.ac.th>
2024-03-09 18:15:00 +0700

    แก้ไข url connection string

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -3,6 +3,6 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> => mongoose.connect('mongodb://localhost:27017/work'),
+    useFactory: (): Promise<typeof mongoose> => mongoose.connect('mongodb+srv://adminploy:Abcd1234@cluster0.mofuwu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test'),
   },
 ]
